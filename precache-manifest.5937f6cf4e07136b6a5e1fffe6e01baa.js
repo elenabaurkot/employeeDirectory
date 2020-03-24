651e13b10eecd0cef0b2d6ecaf787cd5f29efcba
elenabaurkot <elenabaurkot@gmail.com>
2020-03-23 23:30:42 -0400

    Updates

---
+++ precache-manifest.5937f6cf4e07136b6a5e1fffe6e01baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55c1549ac8ef20f6c35fd8c74c61f1a5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "338a36bdcee1f3adfcbc",
+    "url": "/my-app/static/css/main.12f5481a.chunk.css"
+  },
+  {
+    "revision": "522a8ed07b6a49793b22",
+    "url": "/my-app/static/js/2.9ffbf934.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.9ffbf934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "338a36bdcee1f3adfcbc",
+    "url": "/my-app/static/js/main.82145325.chunk.js"
+  },
+  {
+    "revision": "20b26ff37ec4a75f51a8",
+    "url": "/my-app/static/js/runtime-main.7c62e6bd.js"
+  }
+]);
